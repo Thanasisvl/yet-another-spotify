73daff208a7e36f19aef405fa4a804f8d9e06949
Thanos <thanasisvlassis@hotmail.com>
2021-02-26 20:34:57 +0200

    Added base file structure

---
+++ src/main/java/com/threeamigos/music/yetanotherspotify/YetAnotherSpotifyApplication.java
@@ -0,0 +1,13 @@
+package com.threeamigos.music.yetanotherspotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetAnotherSpotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YetAnotherSpotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/threeamigos/music/yetanotherspotify/YetAnotherSpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threeamigos.music.yetanotherspotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetAnotherSpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
